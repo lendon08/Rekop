32f05e7bca7e24ce1a37c3424daa300b6201c992
Lendon Ato <lendon.n.ato@gmail.com>
2024-02-29 04:50:33 +0800

    Added loading

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,13 +17,28 @@
 
     <body class="bg-gray-50 dark:bg-gray-800">
                 
+        <div role="status" class="z-10 absolute -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" >
+            <svg aria-hidden="true" class="w-20 h-20 text-gray-200 animate-spin dark:text-gray-600 fill-black dark:fill-white" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <g transform="rotate(30 50 50)" style="transform:matrix(0.866025, 0.5, -0.5, 0.866025, 31.6987, -18.3013);" ><rect x="47" y="24" rx="3" ry="6" width="6" height="12"  opacity="0.166667" style="opacity:0.166667;" ></rect></g>
+                <g transform="rotate(60 50 50)" style="transform:matrix(0.5, 0.866025, -0.866025, 0.5, 68.3013, -18.3013);" ><rect x="47" y="24" rx="3" ry="6" width="6" height="12"  opacity="0.25" style="opacity:0.25;" ></rect></g>
+                <g transform="rotate(90 50 50)" style="transform:matrix(0, 1, -1, 0, 100, 0);" ><rect x="47" y="24" rx="3" ry="6" width="6" height="12"  opacity="0.333334" style="opacity:0.333334;" ></rect></g>
+                <g transform="rotate(120 50 50)" style="transform:matrix(-0.5, 0.866025, -0.866025, -0.5, 118.301, 31.6987);" ><rect x="47" y="24" rx="3" ry="6" width="6" height="12"  opacity="0.416667" style="opacity:0.416667;" ></rect></g>
+                <g transform="rotate(150 50 50)" style="transform:matrix(-0.866025, 0.5, -0.5, -0.866025, 118.301, 68.3013);" ><rect x="47" y="24" rx="3" ry="6" width="6" height="12"  opacity="0.5" style="opacity:0.5;" ></rect></g>
+                <g transform="rotate(180 50 50)" style="transform:matrix(-1, 0, 0, -1, 100, 100);" ><rect x="47" y="24" rx="3" ry="6" width="6" height="12"  opacity="0.583334" style="opacity:0.583334;" ></rect></g>
+                <g transform="rotate(210 50 50)" style="transform:matrix(-0.866025, -0.5, 0.5, -0.866025, 68.3013, 118.301);" ><rect x="47" y="24" rx="3" ry="6" width="6" height="12"  opacity="0.666667" style="opacity:0.666667;" ></rect></g>
+                <g transform="rotate(240 50 50)" style="transform:matrix(-0.5, -0.866025, 0.866025, -0.5, 31.6987, 118.301);" ><rect x="47" y="24" rx="3" ry="6" width="6" height="12"  opacity="0.75" style="opacity:0.75;" ></rect></g>
+                <g transform="rotate(270 50 50)" style="transform:matrix(0, -1, 1, 0, 0, 100);" ><rect x="47" y="24" rx="3" ry="6" width="6" height="12"  opacity="0.833334" style="opacity:0.833334;" ></rect></g>
+                <g transform="rotate(300 50 50)" style="transform:matrix(0.5, -0.866025, 0.866025, 0.5, -18.3013, 68.3013);" ><rect x="47" y="24" rx="3" ry="6" width="6" height="12"  opacity="0.916667" style="opacity:0.916667;" ></rect></g>
+                <g transform="rotate(330 50 50)" style="transform:matrix(0.866025, -0.5, 0.5, 0.866025, -18.3013, 31.6987);" ><rect x="47" y="24" rx="3" ry="6" width="6" height="12"  opacity="1" style="" ></rect></g></svg>
+            <span class="sr-only">Loading...</span>
+        </div>
         @include('partials.header')
 
         <div class="flex pt-16 overflow-hidden bg-gray-50 dark:bg-gray-900">
 
             @include('partials.sidebar')
  
-            <div id="main-content" class="relative w-full h-full overflow-y-auto bg-gray-50 lg:ml-16 lg:mr-4 dark:bg-gray-900">
+            <div id="main-content" class="relative w-full h-full overflow-y-auto bg-gray-50 lg:ml-16 lg:mx-2 dark:bg-gray-900">
            
                 {{ $slot }}
 
--- resources/views/livewire/pages/dashboard.blade.php
+++ resources/views/livewire/pages/dashboard.blade.php
@@ -5,10 +5,8 @@
         <!-- Upper -->
         <div class="flex items-center justify-between mb-4">
           <div class="flex-shrink-0">
-            <span class="text-base leading-none text-gray-900 sm:text-xl dark:text-white">Total Calls</span>
-            
+            <span class="text-base leading-none text-gray-900 sm:text-xl dark:text-white">Total First-time Callers  </span>
           </div>
-         
           <div class="flex items-center justify-end flex-1 text-base font-medium text-green-500 dark:text-green-400">
             <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
               <path fill-rule="evenodd" d="M5.293 7.707a1 1 0 010-1.414l4-4a1 1 0 011.414 0l4 4a1 1 0 01-1.414 1.414L11 5.414V17a1 1 0 11-2 0V5.414L6.707 7.707a1 1 0 01-1.414 0z" clip-rule="evenodd"></path>
@@ -18,15 +16,16 @@
             <h3 class="text-base font-light text-gray-500 dark:text-gray-400">| Last 7 days</h3>
           </div>
         </div>
-        <hr class="h-px my-2 bg-gray-200 border-0 dark:bg-gray-700">
+        <hr class="h-px my-8 bg-gray-200 border-0 dark:bg-gray-700">
         <!-- mid center -->
         <div class="flex justify-center pt-10">
-          <span class="text-5xl font-semibold leading-none text-gray-800 sm:text-5xl dark:text-white">23</span>  
+          <span class="text-5xl font-semibold leading-none text-gray-800 sm:text-5xl dark:text-white">10</span>  
         </div>
 
         <div class="flex justify-center">
-          <span class="text-xl font-bold leading-none text-gray-800 sm:text-2xl dark:text-white">Calls</span>
+          <span class="text-xl font-bold leading-none text-gray-800 sm:text-2xl dark:text-white">First-time Callers</span>
         </div>
+        <!-- Link -->
         <div class="flex justify-end pt-10">
           <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-gray-800 dark:text-white rounded-lg hover:text-black hover:font-bold
           focus:text-black focus:font-bold">
@@ -36,9 +35,7 @@
             </svg>
           </a>
         </div>
-
       </div>
-
 
       <div class="p-4 bg-white border border-gray-200 rounded-lg shadow-sm 2xl:col-span-1 dark:border-gray-700 sm:p-6 dark:bg-gray-800">
         <!-- Upper -->
@@ -96,13 +93,16 @@
         </div>
         <hr class="h-px my-8 bg-gray-200 border-0 dark:bg-gray-700">
         <!-- mid center -->
-        <div class="w-full mt-5 space-y- text-gray-900 bg-white border border-gray-200 rounded-lg dark:bg-gray-800 dark:border-gray-600 dark:text-white">
-            <button type="button" class="relative inline-flex items-center w-full px-4 py-2 text-sm font-medium border-b border-gray-200 rounded-t-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:border-gray-600 dark:hover:bg-gray-600 dark:hover:text-white dark:focus:ring-gray-500 dark:focus:text-white">
-              <svg class="w-[16px] h-[16px] text-purple-900 dark:text-purple-900" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 19 18">
-                <path d="M18 13.446a3.02 3.02 0 0 0-.946-1.985l-1.4-1.4a3.054 3.054 0 0 0-4.218 0l-.7.7a.983.983 0 0 1-1.39 0l-2.1-2.1a.983.983 0 0 1 0-1.389l.7-.7a2.98 2.98 0 0 0 0-4.217l-1.4-1.4a2.824 2.824 0 0 0-4.218 0c-3.619 3.619-3 8.229 1.752 12.979C6.785 16.639 9.45 18 11.912 18a7.175 7.175 0 0 0 5.139-2.325A2.9 2.9 0 0 0 18 13.446Z"/>
-              </svg>
-                 Outbound . (__% of calls today)
-            </button>
+        <div class="flex flex-col w-full mt-5 space-y-2 text-gray-900 bg-white dark:bg-gray-800 dark:border-gray-600 dark:text-white h-1/3">
+            <div>
+                <button type="button" class="relative inline-flex items-center w-full px-4 py-2 text-sm font-medium border-b border-gray-200 rounded-t-lg hover:bg-gray-100 hover:text-blue-700 
+              focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:border-gray-600 dark:hover:bg-gray-600 dark:hover:text-white dark:focus:ring-gray-500 dark:focus:text-white">
+                <svg class="w-[16px] h-[16px] text-purple-900 dark:text-purple-900" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 19 18">
+                  <path d="M18 13.446a3.02 3.02 0 0 0-.946-1.985l-1.4-1.4a3.054 3.054 0 0 0-4.218 0l-.7.7a.983.983 0 0 1-1.39 0l-2.1-2.1a.983.983 0 0 1 0-1.389l.7-.7a2.98 2.98 0 0 0 0-4.217l-1.4-1.4a2.824 2.824 0 0 0-4.218 0c-3.619 3.619-3 8.229 1.752 12.979C6.785 16.639 9.45 18 11.912 18a7.175 7.175 0 0 0 5.139-2.325A2.9 2.9 0 0 0 18 13.446Z"/>
+                </svg>
+                  Outbound . (__% of calls today)
+              </button>
+            </div>
             <button type="button" class="relative inline-flex items-center w-full px-4 py-2 text-sm font-medium border-b border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:border-gray-600 dark:hover:bg-gray-600 dark:hover:text-white dark:focus:ring-gray-500 dark:focus:text-white">
               <svg class="w-[16px] h-[16px] text-green-900 dark:text-green-900" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 19 18">
                 <path d="M18 13.446a3.02 3.02 0 0 0-.946-1.985l-1.4-1.4a3.054 3.054 0 0 0-4.218 0l-.7.7a.983.983 0 0 1-1.39 0l-2.1-2.1a.983.983 0 0 1 0-1.389l.7-.7a2.98 2.98 0 0 0 0-4.217l-1.4-1.4a2.824 2.824 0 0 0-4.218 0c-3.619 3.619-3 8.229 1.752 12.979C6.785 16.639 9.45 18 11.912 18a7.175 7.175 0 0 0 5.139-2.325A2.9 2.9 0 0 0 18 13.446Z"/>
