4634d45a3d25f0d112396d81cc14e3e3ff105e61
Lendon Ato <46077615+lendon08@users.noreply.github.com>
2025-03-26 02:11:15 +0800

    update the seeder to make it easier.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,9 +4,11 @@ namespace Database\Seeders;
 
 use App\Models\Company;
 use App\Models\User;
+use App\Models\Phonenumbers;
+use App\Integrations\SignalWire;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Testing\Fakes\Fake;
+use App\Services\PhoneFormatService;
+
 
 class DatabaseSeeder extends Seeder
 {
@@ -20,6 +22,21 @@ class DatabaseSeeder extends Seeder
         Company::factory(10)->create();
         User::factory()->create();
 
+
+        $phoneNumbers = SignalWire::http('/api/relay/rest/phone_numbers');
+
+        foreach ($phoneNumbers['data'] as $phoneNumber) {
+            if (Phonenumbers::where('id', $phoneNumber['id'])->exists()) {
+                continue;
+            }
+
+            Phonenumbers::create([
+                'id' => $phoneNumber['id'],
+                'name' => $phoneNumber['name'],
+                'number' => PhoneFormatService::format($phoneNumber['number']),
+                'company_id' => 1,
+            ]);
+        }
 
         $this->call([
             RegionSeeder::class,
